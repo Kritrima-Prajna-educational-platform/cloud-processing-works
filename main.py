6e138cfd1a27f17fe576b084dffcf73c38a046dc
TechForEverybody <Pankajsinghat95@gmail.com>
2025-08-28 22:52:29 +0530

    sync

--- main.py
+++ main.py
@@ -46,5 +46,7 @@ for lesson in lessons:
                         lesson[key]['content'][language][index]['description'] = translated_text
                         lesson[key]['content'][language][index]['audio'] = audio_url
                         index += 1
+                        break
+    break
 with open('./final.lessons.json', 'w', encoding='utf-8') as outfile:
     json.dump(lessons, outfile, ensure_ascii=False, indent=4)
