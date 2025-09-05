2c921343dd46853fb2c8fbd3d1a94d4f72de06ed
TechForEverybody <Pankajsinghat95@gmail.com>
2025-09-05 08:47:37 +0530

    sync

--- main.py
+++ main.py
@@ -20,36 +20,42 @@ language_to_translate = {
     "portuguese": "pt",
     "vietnamese": "vi",
     "english": "en",
-    "default": "en",
+    "gujarati": "gu",
+    "bahasa": "id",
+    "arabic": "ar",
+    "bangla": "bn",
+    "urdu": "ur",
 }
 
 keys_to_translate = ['circuitInstructions', 'codeInstructions', "iotInstructions"]
+
 lesson_number=0
 print(f"Total Lessons: {len(lessons)}")
 for lesson in lessons:
     print(lesson['name'])
     for key in keys_to_translate:
         if key in lesson:
-            print(f"  Translating {key}...")
+            # print(f"  Translating {key}...")
             for language in language_to_translate.keys():
                 # print(lesson[key]['content'], key)
                 # if(len(lesson[key]['content'].keys())>1):
                 #     lesson[key]['content'][language] = lesson[key]['content']['default']
                 if language in lesson[key]['content']:
+                    print(f"    Language: {language}")
                     index=0
                     for step in lesson[key]['content'][language]:
-                        print(f"      Step [{index}]: {step['description']}")
+                        # print(f"      Step [{index}]: {step['description']}")
                         language_code = language_to_translate[language]
                         translated_text = translate_text(step['description'], target_language=language_code)
                         audio_url = generate_audio(translated_text, target_language=language_code)
                         # translated_text = lesson[key]['content']['default'][index]['description']
-                        print(f"      Translated [{language}]: {translated_text}")
-                        print(f"      Audio URL [{language}]: {audio_url}")
+                        # print(f"      Translated [{language}]: {translated_text}")
+                        # print(f"      Audio URL [{language}]: {audio_url}")
                         lesson[key]['content'][language][index]['description'] = translated_text
                         lesson[key]['content'][language][index]['audio'] = audio_url
                         index += 1
     print(f"Completed Lesson {lesson_number}\n")
     lesson_number += 1
 print("All Lessons Completed")
-with open('./updated.json', 'w', encoding='utf-8') as outfile:
+with open('./updated-lessons.json', 'w', encoding='utf-8') as outfile:
     json.dump(lessons, outfile, ensure_ascii=False, indent=4)
